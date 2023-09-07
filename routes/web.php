7e5355f86eed2e3d23bf3be488658aaf0863fcd7
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-09-07 10:37:08 +0200

    Extra files deletion to request the pull request

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\DishController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('landingPage');
-});
-
-Route::get('/user/login', function () {
-    return view('login');
-});
-
-Route::get('/user/signup', function () {
-    return view('signup');
-});
-
-Route::get('/about-us', function () {
-    return view('aboutus');
-});
 
 Route::resource('dish', DishController::class);
 
